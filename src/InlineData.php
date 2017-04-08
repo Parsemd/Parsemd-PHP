9624f8aac9d9fe00feceb90c039d40912cd3f9b5
Aidan Woods <aidantwoods@gmail.com>
2017-04-08 14:09:19 +0100

    add InlineData object

---
+++ src/InlineData.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Aidantwoods\Phpmd;
+
+use Aidantwoods\Phpmd\Lines\Line;
+
+class InlineData
+{
+    private $Line,
+            $Inline;
+
+    public function __construct(Line $Line, Inline $Inline)
+    {
+        $this->Line   = clone($Line);
+        $this->Inline = $Inline;
+    }
+
+    public function start() : int
+    {
+        return $this->Line->key();
+    }
+
+    public function end() : int
+    {
+        return $this->start() + $this->Inline->getWidth();
+    }
+
+    public function textStart() : int
+    {
+        return $this->start() + $this->Inline->getTextStart();
+    }
+
+    public function textEnd() : int
+    {
+        return $this->textStart() + $this->Inline->getTextWidth();
+    }
+
+    public function getInline() : Inline
+    {
+        return $this->Inline;
+    }
+
+    public function getLine() : Line
+    {
+        return clone($this->Line);
+    }
+}
