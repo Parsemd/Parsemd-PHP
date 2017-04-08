cc287f2675282f5a74715144da6b3911aa149783
Aidan Woods <aidantwoods@gmail.com>
2017-04-08 14:08:47 +0100

    add inline extender

---
+++ src/InlineExtender.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Aidantwoods\Phpmd;
+
+use Aidantwoods\Phpmd\Lines\Line;
+
+abstract class InlineExtender
+{
+    private static function squashInlines(array $Inlines) : Line
+    {
+        $NewLine = new Line;
+
+        $start = $Inlines[0]->start();
+        $end   = $Inlines[1]->end();
+        $Line  = $Inlines[0]->getLine();
+
+        $NewLine->append($Line->substr(0, $start));
+        $NewLine->append(str_repeat("\0", $end - $start));
+        $NewLine->append($Line->substr($end));
+
+        return $NewLine;
+    }
+
+    private static function unsquashInlines(
+        array  $Inlines,
+        Inline $Outer
+    ) : ?Inline
+    {
+        $Outer = clone($Outer);
+
+        $text = (string) $Outer->getElement()->getContent();
+
+        $start = $Inlines[0]->start();
+        $end   = $Inlines[1]->end();
+        $Line  = $Inlines[0]->getLine();
+
+        $n = strpos ($text, "\0");
+        $l = strrpos($text, "\0");
+
+        if ($n === false)
+        {
+            return null;
+        }
+
+        $Outer->getElement()->getContent()->pop();
+
+        $newText  = substr($text, 0, $n);
+        $newText .= $Line->substr($start, $end);
+
+        if ($l + 1 < strlen($text))
+        {
+            $newText .= substr($text, $l + 1);
+        }
+
+        $Outer->getElement()->appendContent($newText);
+
+        return $Outer;
+    }
+
+    public static function extend(
+        InlineData $InlineData,
+        array      $Interrupts
+    ) : ?InlineData
+    {
+        $squashedLine = self::squashInlines($Interrupts);
+        $squashedLine->jump($InlineData->start());
+
+        if ($Inline = $InlineData->getInline()::parse($squashedLine))
+        {
+            if ($Inline = self::unsquashInlines($Interrupts, $Inline))
+            {
+                return new InlineData($InlineData->getLine(), $Inline);
+            }
+        }
+
+        return null;
+    }
+}
