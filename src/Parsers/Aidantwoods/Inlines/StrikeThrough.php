3092417896d04d546e5f2b4ecab63237c98611bc
Aidan Woods <aidantwoods@gmail.com>
2017-05-13 15:49:25 +0100

    trim unused imports

--- src/Parsers/Aidantwoods/Inlines/StrikeThrough.php
+++ src/Parsers/Aidantwoods/Inlines/StrikeThrough.php
@@ -3,12 +3,7 @@ declare(strict_types=1);
 
 namespace Parsemd\Parsemd\Parsers\Aidantwoods\Inlines;
 
-use Parsemd\Parsemd\Elements\InlineElement;
-use Parsemd\Parsemd\Lines\Line;
-
 use Parsemd\Parsemd\Parsers\Inline;
-use Parsemd\Parsemd\Parsers\Core\Inlines\AbstractInline;
-
 use Parsemd\Parsemd\Parsers\CommonMark\Abstractions\Inlines\Emphasis;
 
 class StrikeThrough extends Emphasis implements Inline
